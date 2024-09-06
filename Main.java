fec365f34a8ea43cc5338363842b7584013aa9c0
Ahmad Yahya <moodi89my@gmail.com>
2024-09-06 20:34:31 +0200

    Laget kode til å opprette ett rom

---
+++ Main.java
@@ -0,0 +1,38 @@
+import com.google.gson.Gson;
+import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Room> rooms = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the name of the room: ");
+        String roomName = scanner.nextLine();
+
+        rooms.add(new Room(roomName));
+        System.out.println("You have added room: " + roomName + " to your list.");
+
+        Gson gson = new Gson();
+
+        // lager rom i json fil.
+        File file = new File("rooms/rooms.json");
+
+        try (FileWriter writer = new FileWriter(file)) {
+            gson.toJson(rooms, writer);
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Skriv ut rom list
+        System.out.println("List of rooms: ");
+        for (Room room : rooms) {
+            System.out.println(room.getName());
+        }
+    }
+}
